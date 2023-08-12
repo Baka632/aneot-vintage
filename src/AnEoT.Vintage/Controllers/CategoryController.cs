ed08fbd55a0f851a4cfc9d1796abc211fefe25be
Baka632 <stevemc123456@outlook.com>
2023-08-12 17:42:47 +0800

    添加基础文件

---
+++ src/AnEoT.Vintage/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnEoT.Vintage.Controllers
+{
+    /// <summary>
+    /// 分类页面控制器
+    /// </summary>
+    public class CategoryController : Controller
+    {
+        /// <summary>
+        /// 显示分类页面
+        /// </summary>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
