b734714cc26f78255eca33f9a9fbdf6c0fe6e6b4
Baka632 <stevemc123456@outlook.com>
2024-09-29 16:15:34 +0800

    - 修复网页页首不能正常居中的问题 - 移除 API 项目与 Swagger

--- src/AnEoT.Vintage/Program.cs
+++ src/AnEoT.Vintage/Program.cs
@@ -6,12 +6,9 @@ using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.WebEncoders;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 using Westwind.AspNetCore.Markdown;
-using SixLabors.ImageSharp;
 
 namespace AnEoT.Vintage;
 
@@ -31,11 +28,11 @@ public class Program
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         #region 第一步：读取应用程序配置
-        //确定是否启用WebP图像转换功能
+        // 确定是否启用 WebP 图像转换功能
         _ = bool.TryParse(builder.Configuration["ConvertWebP"], out bool convertWebP);
 
         #region 静态页面
-        //设置静态页面的导出位置
+        // 设置静态页面的导出位置
         string staticWebSiteOutputPath;
 
         {
@@ -52,12 +49,12 @@ public class Program
             }
         }
 
-        //确定是否生成静态网页
+        // 确定是否生成静态网页
         bool generateStaticWebSite = args.HasExitWhenDoneArg();
         #endregion
 
         #region RSS
-        //设置RSS源的基Uri
+        // 设置 RSS 源的基 Uri
         string rssBaseUri;
 
         {
@@ -68,7 +65,7 @@ public class Program
             {
                 rssBaseUri = hostUrls?.FirstOrDefault(x => x.StartsWith(Uri.UriSchemeHttp, StringComparison.OrdinalIgnoreCase))
                         ?? hostUrls?.FirstOrDefault(x => x.StartsWith(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase))
-                        ?? throw new InvalidOperationException("现在无法获取到基Uri");
+                        ?? throw new InvalidOperationException("现在无法获取到基 Uri");
             }
             else
             {
@@ -76,20 +73,20 @@ public class Program
             }
         }
 
-        //确定是否生成完整的 RSS 源 （包含全部文章）
+        // 确定是否生成完整的 RSS 源 （包含全部文章）
         _ = bool.TryParse(builder.Configuration["RssIncludeAllArticles"], out bool rssIncludeAllArticles);
         
-        //确定生成的 RSS 源是否包含样式
+        // 确定生成的 RSS 源是否包含样式
         _ = bool.TryParse(builder.Configuration["RssAddCssStyle"], out bool rssAddCssStyle);
         #endregion
         #endregion
 
         #region 第二步：向依赖注入容器添加服务
 
-        //添加Markdown解析服务
+        // 添加 Markdown 解析服务
         builder.Services.AddMarkdown(config =>
         {
-            //设置Markdown中间件的工作文件夹
+            // 设置 Markdown 中间件的工作文件夹
             config.AddMarkdownProcessingFolder("/");
             config.ConfigureMarkdigPipeline = builder =>
             {
@@ -104,22 +101,8 @@ public class Program
         });
         builder.Services.AddControllersWithViews()
             .AddApplicationPart(typeof(MarkdownPageProcessorMiddleware).Assembly);
-        
-        //添加Swagger API文档
-        builder.Services.AddSwaggerGen((options) =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Version = "v1",
-                Title = "AnEoT API",
-                Description = "《回归线》的公共API"
-            });
 
-            string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-        });
-
-        //注入IUrlHelper服务
+        // 注入 IUrlHelper 服务
         builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>()
             .AddScoped(provider =>
             {
@@ -149,7 +132,7 @@ public class Program
         #endregion
 
         #region 第三步：配置 HTTP 请求管道 + 额外工作
-        //配置Uri重写
+        // 配置 Uri 重写
         RewriteOptions rewriteOptions = new RewriteOptions()
             .Add(context =>
             {
@@ -178,7 +161,7 @@ public class Program
                 }
             });
         
-        //启用Uri重写
+        // 启用 Uri 重写
         app.UseRewriter(rewriteOptions);
 
         if (app.Environment.IsDevelopment())
@@ -191,10 +174,7 @@ public class Program
         }
         app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
-        app.UseSwagger();
-        app.UseSwaggerUI();
-
-        //让Markdown中间件能够自动获取到期刊页面的README.md文件
+        // 让 Markdown 中间件能够自动获取到期刊页面的 README.md 文件
         app.UseDefaultFiles(new DefaultFilesOptions()
         {
             DefaultFileNames = defaultFileNames
@@ -204,34 +184,7 @@ public class Program
 
         app.UseMarkdown();
 
-        if (convertWebP)
-        {
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                OnPrepareResponse = async context =>
-                {
-                    string acceptHeader = context.Context.Request.Headers.Accept.ToString();
-
-                    if (generateStaticWebSite != true
-                        && !acceptHeader.Contains("image/webp", StringComparison.OrdinalIgnoreCase)
-                        && context.File.Name.EndsWith(".webp", StringComparison.OrdinalIgnoreCase)
-                        && context.Context.Response.StatusCode != StatusCodes.Status304NotModified
-                        && context.File.PhysicalPath is not null)
-                    {
-                        context.Context.Response.Headers.Remove("Content-Length");
-
-                        using Image image = Image.Load(context.File.PhysicalPath);
-                        context.Context.Response.ContentType = "image/jpeg";
-
-                        await image.SaveAsJpegAsync(context.Context.Response.Body);
-                    }
-                }
-            });
-        }
-        else
-        {
-            app.UseStaticFiles();
-        }
+        app.UseStaticFiles();
 
         app.UseRouting();
         app.MapDefaultControllerRoute();
