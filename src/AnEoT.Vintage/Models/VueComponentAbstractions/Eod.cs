6f70c327dc141aa9f12329a988a5dec8cfe99a74
Baka632 <stevemc123456@outlook.com>
2024-12-16 21:59:51 +0800

    同步至 Vol. 24 - 2024 年 12 月号：从前有座巴别塔

---
+++ src/AnEoT.Vintage/Models/VueComponentAbstractions/Eod.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace AnEoT.Vintage.Models.VueComponentAbstractions;
+
+/// <summary>
+/// 文章结束元素（&lt;eod /&gt;）。
+/// </summary>
+public static class Eod
+{
+    /// <summary>
+    /// 元素标签名。
+    /// </summary>
+    public const string TagName = "EOD";
+
+    /// <summary>
+    /// 元素 HTML 模板
+    /// </summary>
+    private const string Template = """<span class="no-interact{0}"><img id="eod-image-element" src="/eod.jpg" /></span>""";
+
+    private readonly static CompositeFormat TemplateFormat = CompositeFormat.Parse(Template);
+
+    /// <summary>
+    /// 获取 <see cref="Eod"/> 的 HTML。
+    /// </summary>
+    /// <param name="optionalClassName">可选的类名，以空格分割。</param>
+    /// <returns>构造好的 <see cref="Eod"/> 的 HTML</returns>
+    public static string GetHtml(string optionalClassName = "")
+    {
+        return string.Format(CultureInfo.InvariantCulture, TemplateFormat, $" {optionalClassName}");
+    }
+}
