ebcd1142de0e2566d988d06d66939a34588e8a49
Baka632 <stevemc123456@outlook.com>
2025-04-10 21:39:59 +0800

    - 将 Feed 生成中的霞鹜文楷字体链接提取为常量 - 更新用于项目说明的 src/README.md - 使用简化的项目输出布局 - 更新生成脚本

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh --version latest -InstallDir ./dotnet
-./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o output
-./dotnet/dotnet run --configuration Release --project ./src/AnEoT.Vintage.Tool/AnEoT.Vintage.Tool.csproj remove-unnecessary-component --webroot-path ./src/AnEoT.Vintage/wwwroot
-./dotnet/dotnet run --configuration Release --project ./src/AnEoT.Vintage/AnEoT.Vintage.csproj -- static-only --ConvertWebP true --urls "http://localhost:5048" --RssBaseUri "https://aneot-vintage.baka632.com/" --RssIncludeAllArticles true --RssAddCssStyle true
+# 假定运行环境已经安装了 dotnet
+
+dotnet run --configuration Release \
+ --project ./src/AnEoT.Vintage.Tool/AnEoT.Vintage.Tool.csproj remove-unnecessary-component \
+ --webroot-path ./src/AnEoT.Vintage/wwwroot
+
+dotnet run --configuration Release \
+ --project ./src/AnEoT.Vintage/AnEoT.Vintage.csproj \
+ -- static-only \
+ --urls "http://localhost:5048" \
+ --BaseUri "http://aneot-vintage.arktca.com/"
+
+dotnet publish ./src/AnEoT.Vintage.StaticServer/AnEoT.Vintage.StaticServer.csproj \
+ -r linux-x64 \
+ --self-contained
