9aee40f295571f342ec0bd7aeba1362c300a3275
Baka632 <stevemc123456@outlook.com>
2025-04-11 22:43:56 +0800

    为反向代理服务添加自动 HTTPS 升级

--- src/AnEoT.Vintage.ReverseProxy/Program.cs
+++ src/AnEoT.Vintage.ReverseProxy/Program.cs
@@ -1,7 +1,22 @@
+using AnEoT.Vintage.ReverseProxy;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddHsts(options =>
+{
+    options.Preload = true;
+    options.IncludeSubDomains = true;
+    options.MaxAge = TimeSpan.FromMinutes(10);
+});
 
 WebApplication app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseMiddleware<TryHttpsUpgradeMiddleware>();
 
 app.MapReverseProxy();
 
