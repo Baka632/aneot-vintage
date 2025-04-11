ce4c885e97161c69af6d65b6e792928529d486e3
Baka632 <stevemc123456@outlook.com>
2025-04-11 23:12:16 +0800

    取消使用中间件，改为直接使用 Use 方法，并且不再在 HTTPS 升级过程中自动探测服务端口，而是统一使用 443 端口

--- src/AnEoT.Vintage.ReverseProxy/Program.cs
+++ src/AnEoT.Vintage.ReverseProxy/Program.cs
@@ -1,4 +1,6 @@
 using AnEoT.Vintage.ReverseProxy;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
@@ -16,7 +18,28 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseMiddleware<TryHttpsUpgradeMiddleware>();
+app.Use(async (context, next) =>
+{
+    if (!context.Request.IsHttps && context.Request.Headers.UpgradeInsecureRequests.Equals("1"))
+    {
+        HttpRequest request = context.Request;
+        string redirectUrl = UriHelper.BuildAbsolute(
+            "https",
+            request.Host,
+            request.PathBase,
+            request.Path,
+            request.QueryString);
+
+        context.Response.StatusCode = StatusCodes.Status307TemporaryRedirect;
+        context.Response.Headers.Location = redirectUrl;
+        context.Response.Headers.Vary = "Upgrade-Insecure-Requests";
+        return;
+    }
+    else
+    {
+        await next(context);
+    }
+});
 
 app.MapReverseProxy();
 
